6ea32294da043680d3c2aeecb34983f2f4f00355
Shravan-kumar-45 <shravance0061@gmail.com>
2024-12-25 17:57:29 +0530

    Revision

--- PythonBasics.py
+++ PythonBasics.py
@@ -1,1 +1,35 @@
+from operator import truediv
+
 print("Hello World")
+# Predefined Objects , keywords & Identifier
+print("Hello World") # Open and Close brackets
+
+a = 3 # Here A is a variable
+# variable are placeholder where we will keep variety of data which we can use to write code.
+
+print(type(a)) # here a is a integer
+
+b= 4.4 # it is a float
+print(type(b))
+
+c= " shravan"
+print(type(c))
+d= True
+print(type(d))
+
+e = True - False
+print("Type of e",type(e))
+
+f = 34 -67j
+print(type(f))
+
+print(f.real) # to  find real part of the complex number
+print(f.imag) # to find imaginary part of complex number
+
+'''Rules of defining variable start with letter or underscore 
+12iinn in vaild
+_renfe12 vaild'''
+
+# Always try to name variable which make sense
+
+
